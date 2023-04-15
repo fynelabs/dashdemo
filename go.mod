0dad5da165ca6b602750c8cfddfb7e4fd282b1cb
Andy Williams <andy@ndy.xyz>
2023-04-15 21:08:34 +0100

    Enable self-update

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/fyne-io/examples
+module github.com/FyneLabs/dashdemo
 
 go 1.12
 
-require fyne.io/fyne/v2 v2.3.1
+require (
+	fyne.io/fyne/v2 v2.3.1
+	github.com/fynelabs/fyneselfupdate v0.1.0
+	github.com/fynelabs/selfupdate v0.2.0
+)
--- main.go
+++ main.go
@@ -4,12 +4,34 @@
 package main
 
 import (
+	"crypto/ed25519"
+	"log"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
+
+	"github.com/fynelabs/fyneselfupdate"
+	"github.com/fynelabs/selfupdate"
 )
+
+// selfManage turns on automatic updates
+func selfManage(a fyne.App, w fyne.Window) {
+	publicKey := ed25519.PublicKey{174, 168, 8, 15, 255, 74, 75, 30, 141, 156, 193, 40, 49, 94, 201, 184, 219, 38, 229, 172, 110, 43, 136, 88, 220, 230, 218, 185, 28, 155, 102, 230}
+
+	httpSource := selfupdate.NewHTTPSource(nil, "https://geoffrey-artefacts.fynelabs.com/self-update/97/97e1436a-ed9b-451e-bc1d-0788875a2840/{{.OS}}-{{.Arch}}/{{.Executable}}{{.Ext}}")
+
+	config := fyneselfupdate.NewConfig(a, w, httpSource, selfupdate.Schedule{FetchOnStart: true, Interval: time.Second * 15}, publicKey)
+
+	_, err := selfupdate.Manage(config)
+	if err != nil {
+		log.Println("Error while setting up update manager: ", err)
+		return
+	}
+}
 
 func main() {
 	a := app.New()
@@ -24,5 +46,7 @@ func main() {
 		container.NewCenter(temp),
 		speedo(),
 		container.NewCenter(dir)))
+
+	selfManage(a, w)
 	w.ShowAndRun()
 }
